b9e1da7779f30c6d773d9ac6e0375732fd32c9ed
HM-Jang <runas3078@naver.com>
2015-10-16 21:51:54 +0900

    CategoryFragment 내, Category Menu Load 분리
    #장현민

--- PetboxShop/app/src/main/java/com/petbox/shop/Fragment/Category/CategoryFragment.java
+++ PetboxShop/app/src/main/java/com/petbox/shop/Fragment/Category/CategoryFragment.java
@@ -40,7 +40,7 @@ import java.util.List;
  * Use the {@link CategoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CategoryFragment extends Fragment implements View.OnClickListener, HttpGetDelegate{
+public class CategoryFragment extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -67,11 +67,9 @@ public class CategoryFragment extends Fragment implements View.OnClickListener, 
 
     CategoryDelegate delegate;
 
-    ArrayList<ArrayList<ArrayList<CategoryInfo>>> categoryList;
-    Tree<CategoryInfo> dog_tree;
-    Tree<CategoryInfo> cat_tree;
-
     String param;
+
+    CategoryManager categoryManager;
 
     // TODO: Rename and change types and number of parameters
     public static CategoryFragment newInstance(CategoryDelegate delegate, String param2) {
@@ -101,29 +99,7 @@ public class CategoryFragment extends Fragment implements View.OnClickListener, 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        dog_tree = new Tree<CategoryInfo>();
-        Node<CategoryInfo> dog_root = new Node<CategoryInfo>();
-        CategoryInfo categoryInfo = new CategoryInfo();
-        categoryInfo.name = "애견 카테고리";
-        categoryInfo.category_num = "035";
-        categoryInfo.sort = 0;
-
-        dog_root.setData(categoryInfo);
-        dog_tree.setRoot(dog_root);
-
-        cat_tree = new Tree<CategoryInfo>();
-        Node<CategoryInfo> cat_root = new Node<CategoryInfo>();
-
-        categoryInfo = new CategoryInfo();
-        categoryInfo.name = "애묘 카테고리";
-        categoryInfo.category_num = "036";
-        categoryInfo.sort = 0;
-
-        cat_root.setData(categoryInfo);
-        cat_tree.setRoot(cat_root);
-
-        CategoryManager categoryManager = new CategoryManager();
-
+        categoryManager = new CategoryManager();
 
         View v = inflater.inflate(R.layout.fragment_category, container, false);
 
@@ -417,135 +393,5 @@ public class CategoryFragment extends Fragment implements View.OnClickListener, 
         */
     }
 
-    @Override
-    public void prevRunningHttpGet() {
 
-    }
-
-    @Override
-    public String getUrl() {
-        return Constants.HTTP_URL_CATEGORY_LIST;
-    }
-
-    @Override
-    public List<NameValuePair> getNameValuePairs() {
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-
-        param = "035";
-
-        nameValuePairs.add(new BasicNameValuePair("category", param));
-
-        return nameValuePairs;
-    }
-
-    @Override
-    public void runningHttpGet() {
-
-    }
-
-    @Override
-    public void afterRunningHttpGet(String jsonData) {
-        try{
-            //System.out.println(jsonData);
-
-            JSONArray jsonArray = new JSONArray(jsonData);
-            JSONObject main = jsonArray.getJSONObject(0);
-            JSONArray category_data = main.getJSONArray("category_data");
-            //JSONObject category_data = main.getJSONObject("category_data");
-
-            ArrayList<CategoryInfo> arrCategory1 = new ArrayList<CategoryInfo>();   // 카테고리 글자 6
-            ArrayList<CategoryInfo> arrCategory2 = new ArrayList<CategoryInfo>();   // 카테고리 글자 9
-            ArrayList<CategoryInfo> arrCategory3 = new ArrayList<CategoryInfo>();   // 카테고리 글자 12
-
-            for(int i=0; i< category_data.length(); i++){
-                JSONObject obj = category_data.getJSONObject(i);
-
-                CategoryInfo item = new CategoryInfo();
-
-                item.name = obj.getString("catnm");
-                item.category_num = obj.getString("category");
-                item.sort  = obj.getInt("sort");
-
-                switch(item.category_num.length()){
-                    case 6:
-                        arrCategory1.add(item);
-                        //System.out.println("1차 : " + item.category_num + "// " + item.name);
-                        break;
-
-                    case 9:
-                        arrCategory2.add(item);
-                        //System.out.println("2차 : " + item.category_num + "// " + item.name);
-                        break;
-
-                    case 12:
-                        arrCategory3.add(item);
-                        //System.out.println("3차 : " + item.category_num + "// " + item.name);
-                        break;
-                }
-            }
-
-            //root(1차), Tree의 2차 Depth에 넣음.
-            for(int i=0; i<arrCategory1.size(); i++){
-                Node<CategoryInfo> item = new Node<CategoryInfo>();
-                item.setData(arrCategory1.get(i));
-                dog_tree.add(dog_tree.root, item);
-
-                //System.out.println("1차 : " + item.getData().category_num);
-                //ArrayList<ArrayList<CategoryInfo>> itemList = new ArrayList<ArrayList<CategoryInfo>>();
-            }
-
-            //root(1차), Tree의 3차 Depth에 넣음.
-            for(int i=0; i<arrCategory2.size(); i++){
-                Node<CategoryInfo> nodeItem = new Node<CategoryInfo>();
-                nodeItem.setData(arrCategory2.get(i));
-                CategoryInfo item = arrCategory2.get(i);
-
-                ArrayList<Node<CategoryInfo>> arrList = dog_tree.root.getChildList();
-
-                String split_num = item.category_num.substring(0,6);// 6자리
-
-                //System.out.println("2차(SPLIT) : " + split_num + "// root.childList.size : " +  arrList.size());
-
-                //Depth : 2의 항목들과 비교교
-               for(int j=0; j< arrList.size(); j++){
-                    CategoryInfo parentItem = arrList.get(j).getData();
-
-                   //System.out.println("2차 : " + parentItem.category_num + " - " + split_num);
-
-                    if(parentItem.category_num.equals(split_num)){
-                        arrList.get(j).addChild(nodeItem);
-                        //System.out.println("2차 : " + parentItem.category_num + " - " + item.name+"("+item.category_num+")");
-                        break;
-                    }
-                }
-            }
-
-
-            for(int i=0; i<arrCategory3.size(); i++){
-                Node<CategoryInfo> nodeItem = new Node<CategoryInfo>();
-                nodeItem.setData(arrCategory3.get(i));
-                CategoryInfo item = arrCategory3.get(i);
-
-                String split_num = item.category_num.substring(0, 9);// 6자리
-
-                ArrayList<Node<CategoryInfo>> rootChildList = dog_tree.root.getChildList();
-
-                for(int j=0; j< rootChildList.size(); j++) {
-                    ArrayList<Node<CategoryInfo>> arrList = rootChildList.get(j).getChildList();
-
-                    for (int k = 0; k < arrList.size(); k++) {
-                        CategoryInfo parentItem = arrList.get(k).getData();
-
-                        if (parentItem.category_num.equals(split_num)) {
-                            arrList.get(j).addChild(nodeItem);
-                            //System.out.println("3차 : " + parentItem.category_num + " - " + item.name + "(" + item.category_num + ")");
-                            break;
-                        }
-                    }
-                }
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
 }
